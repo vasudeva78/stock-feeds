9141351640e84371d474be3e7cbb711f255bb348
ajay vasudevan <ajay.vasudevan@bike4fit.co.uk>
2022-08-01 00:23:18 +0100

    working shell script in raspberry pi

---
+++ stock-feed-start-up.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+cd /home/pi/h2-database/h2-2022-06-13/h2/bin
+rm *.out
+java -Xms16m -Xmx16m -Xss512k -XX:+UseConcMarkSweepGC -cp h2-2.1.214.jar org.h2.tools.Server &
+
+sleep 20s
+
+cd /home/pi/stock-feeds/
+rm *.out
+java -Xms64m -Xmx64m -Xss512k -XX:+UseConcMarkSweepGC -Dmicronaut.environments=linux -jar stock-feeds.jar &
